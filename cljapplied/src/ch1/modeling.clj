4ac6035ae6c8d772f2eb977bc68ed712f27ba0d0
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-04-14 17:57:53 +0900

    Records provide some class-like features

--- cljapplied/src/ch1/modeling.clj
+++ cljapplied/src/ch1/modeling.clj
@@ -8,3 +8,25 @@
             })
 
 ;; user> earth
+
+(defrecord Planet [name
+                   moons
+                   volume
+                   mass
+                   aphelion
+                   perihelion
+                   ])
+
+;; Type
+;; user> :Planet
+
+;; Positional factory function
+;; user> (->Planet "Earth" 1 1.08321E12 5.97219E24 152098232 147098290)
+
+;; Map factory function
+;; user> (map->Planet {:name "Earth"
+;;                     :moons 1
+;;                     :volume 1.08321E12
+;;                     :mass 5.97219E24
+;;                     :aphelion 152098232
+;;                     :perihelion 147098290})
