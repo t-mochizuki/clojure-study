80134e5a09e4257016e17ddb728f56e624a40760
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-04-14 19:38:28 +0900

    Build a Money value object

---
+++ cljapplied/src/ch1/money.clj
@@ -0,0 +1,14 @@
+(ns ch1.money)
+
+(declare validate-same-currency)
+
+(defrecord Currency [divisor sym desc])
+
+(defrecord Money [amount ^Currency currency]
+  java.lang.Comparable
+  (compareTo [m1 m2]
+    (validate-same-currency m1 m2)
+    (compare (:amount m1) (:amount m2))))
+
+(def currencies {:usd (->Currency 100 "USD" "US Dollars")
+                 :eur (->Currency 100 "EUR" "Euro")})
