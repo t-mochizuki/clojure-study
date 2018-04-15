a38b4aa4df885a3e6d185d56eb1b3002642aca54
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-04-15 15:58:32 +0900

    Modeling Relationships: identifiers

--- cljapplied/src/ch1/recipe.clj
+++ cljapplied/src/ch1/recipe.clj
@@ -24,3 +24,19 @@
    1))
 
 ;; user> toast
+
+(def people
+  {"p1" (->Person "Alex" "Miller")})
+
+;; user> people
+
+(def recipes
+  {"r1" (->Recipe
+         "Toast"
+         "p1"
+         "Crispy bread"
+         ["Slice of bread"]
+         ["Toast bread in toaster"]
+         1)})
+
+;; user> recipes
