8217c0909c1320395b2fa7bc271181e5591ea71a
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-04-15 17:13:24 +0900

    If the data is valid, s/check returns nil

--- cljapplied/src/ch1/validate.clj
+++ cljapplied/src/ch1/validate.clj
@@ -1,20 +1,23 @@
-(defrecord Recipe
-    [name
-     description
-     ingredients
-     steps
-     servings
+(ns ch1.validate
+  (:require [schema.core :as s]))
+
+(s/defrecord Ingredient
+    [name :- s/Str
+     quntity :- s/Int
+     unit :- s/Keyword
      ])
 
-(defrecord Ingredient
-    [name
-     quntity
-     unit
+(s/defrecord Recipe
+    [name :- s/Str
+     description :- s/Str
+     ingredients :- [Ingredient]
+     steps :- [s/Str]
+     servings :- s/Int
      ])
 
 (def spagetti-tacos
   (map->Recipe
-   {:naem "Spagetti tacos"
+   {:name "Spagetti tacos"
     :description "It's spagetti... in a tacos"
     :ingredients [(->Ingredient "Spagetti" 1 :lb)
                   (->Ingredient "Spagetti sauce" 16 :oz)
@@ -25,4 +28,17 @@
             "Put spagetti in taco shells and serve."]
     :servings 4}))
 
-;; user> spagetti-tacos
+;; user> (require 'ch1.validate)
+;; user> (in-ns 'ch1.validate)
+;; ch1.validate> spagetti-tacos
+
+;; ch1.validate> (s/check Recipe spagetti-tacos)
+;; {:name (not (instance? java.lang.String nil)), :naem disallowed-key}
+;; ch1.validate> (s/check Recipe spagetti-tacos)
+;; nil
+
+;; ch1.validate> (s/defn add-ingredients :- Recipe
+;;                 [recipe :- Recipe & ingredients :- [Ingredient]]
+;;                 (update-in recipe [:ingredients] into ingredients))
+;; #'ch1.validate/add-ingredients
+;; ch1.validate> (doc add-ingredients)
