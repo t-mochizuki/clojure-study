2948cd1c3f0ddf814ef6d3ce576f1470f1c04e4a
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-03-10 11:57:24 +0900

    update it

--- abc139A'.clj
+++ abc139A'.clj
@@ -3,8 +3,10 @@
   ([s t acc]
    (if (nil? s)
      acc
-     (if (= (first s) (first t))
-       (solve (next s) (next t) (inc acc))
-       (solve (next s) (next t) acc)))))
+     (solve (next s)
+            (next t)
+            (if (= (first s) (first t))
+              (inc acc)
+              acc)))))
 
 (println (solve (str (read)) (str (read))))
