334c76b3377dd2a10efd935bd1dc43bc0198894f
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-05-17 16:54:51 +0900

    Add schema namespace

---
+++ lacinia-example/src/lacinia_example/schema.clj
@@ -0,0 +1,19 @@
+(ns lacinia-example.schema
+  (:require
+    [clojure.edn :as edn]
+    [clojure.java.io :as io]
+    [com.walmartlabs.lacinia.schema :as schema]
+    [com.walmartlabs.lacinia.util :as util]))
+
+(defn resolver-map
+  []
+  {:query/numbers (fn [context args value]
+                    [2 3 4])})
+
+(defn load-schema
+  []
+  (-> (io/resource "schema.edn")
+      slurp
+      edn/read-string
+      (util/attach-resolvers (resolver-map))
+      schema/compile))
