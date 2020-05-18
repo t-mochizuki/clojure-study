821633db5064da3bd08be42880ddcfdebcfa02ab
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-05-18 23:06:39 +0900

    Add a picture query

--- lacinia-example/src/lacinia_example/schema.clj
+++ lacinia-example/src/lacinia_example/schema.clj
@@ -5,6 +5,9 @@
     [com.walmartlabs.lacinia.schema :as schema]
     [com.walmartlabs.lacinia.util :as util]))
 
+(defn picture [size]
+  {:width size :height size})
+
 (defn resolver-map
   []
   (let [data (-> (io/resource "data.edn")
@@ -16,7 +19,9 @@
       {:query/numbers (fn [context args value]
                         [2 3 4])
        :query/products (fn [context args value]
-                         products-map)})))
+                         products-map)
+       :query/picture (fn [context args value]
+                        (picture (:size args)))})))
 
 (defn load-schema
   []
